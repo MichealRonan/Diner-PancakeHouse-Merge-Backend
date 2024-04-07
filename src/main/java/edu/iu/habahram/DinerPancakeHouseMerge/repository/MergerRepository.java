08e77469437f97aa04c2db2070e4a1239765f1a2
MichealRonan <132925162+MichealRonan@users.noreply.github.com>
2024-04-07 18:25:28 -0500

    Part 6

---
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/MergerRepository.java
@@ -0,0 +1,19 @@
+package edu.iu.habahram.DinerPancakeHouseMerge.repository;
+
+import edu.iu.habahram.DinerPancakeHouseMerge.model.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class MergerRepository {
+
+
+    public ArrayList<Menu> getTheMenus() {
+        ArrayList<Menu> menus = new ArrayList<>();
+        menus.add(new DinerMenu());
+        menus.add(new PancakeHouseMenu());
+        menus.add(new CafeMenu());
+        return menus;
+    }
+}
